9d93967aacec2f4608cd689ea75bb7cf178d1e70
v1lling <sascha.villing@web.de>
2021-05-05 12:30:15 +0200

    Prepare spotify login

---
+++ lib/models/user.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+class User extends Equatable {
+  User({@required this.id, @required this.name, @required this.avatarImageUrl});
+  final String name;
+  final String avatarImageUrl;
+  final String id;
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    if (json == null) return null;
+    final name = json['display_name'];
+    final avatarImageUrl =
+        json['images'].length != 0 ? json['images'][0]['url'] : null;
+    final id = json['id'];
+    return User(name: name, avatarImageUrl: avatarImageUrl, id: id);
+  }
+
+  Map<String, dynamic> toJson() => {
+        'display_name': name,
+        'images': [
+          {'url': avatarImageUrl}
+        ],
+        'id': id
+      };
+
+  @override
+  List<Object> get props => [name, avatarImageUrl, id];
+}
