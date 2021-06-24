b2698c947d484c2d8bc7afd95d1019a80f086aec
Sascha Villing <sa981vil@htwg-konstanz.de>
2021-06-24 16:08:34 +0200

    only show onboard screen first time

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var localeService = Provider.of<LocaleService>(context);
-    bool isOnboarded = false;
+    Preference<bool> isOnboarded =
+        preferences.getBool('isOnboarded', defaultValue: false);
     return PreferenceBuilder<String>(
       preference: preferences.getString('token', defaultValue: ""),
       builder: (BuildContext context, String token) {
@@ -92,7 +93,7 @@ class App extends StatelessWidget {
                     title: 'Fluttify',
                     theme: notifire.getTheme(),
                     initialRoute: token != "" && token != "initial"
-                        ? isOnboarded
+                        ? isOnboarded.getValue()
                             ? Routes.homeView
                             : Routes.onboardingView
                         : Routes.spotifySignInView,
--- lib/ui/views/onboarding_view.dart
+++ lib/ui/views/onboarding_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:fluttify/ui/views/home_view.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OnboardingView extends StatefulWidget {
   @override
@@ -13,7 +14,9 @@ class OnboardingView extends StatefulWidget {
 class _OnboardingPageState extends State<OnboardingView> {
   final introKey = GlobalKey<IntroductionScreenState>();
 
-  void _onIntroEnd(context) {
+  void _onIntroEnd(context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("isOnboarded", true);
     Navigator.of(context).push(
       MaterialPageRoute(builder: (_) => HomeView()),
     );
