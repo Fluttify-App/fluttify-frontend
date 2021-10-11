7adcabd20103e563fbd295b1f64a83c25ed8ef64
v1lling <sascha.villing@web.de>
2021-10-11 13:07:25 +0200

    Snackbar color green

--- lib/ui/views/playlists_views/edit_playlist_views/edit_playlist_viewmodel.dart
+++ lib/ui/views/playlists_views/edit_playlist_views/edit_playlist_viewmodel.dart
@@ -183,6 +183,7 @@ class EditPlaylistViewModel extends BaseViewModel {
         .then((playlistUpdate) {
       Navigator.of(context).pop(true);
       final snackBar = SnackBar(
+        backgroundColor: Theme.of(context).indicatorColor,
         content: Text(AppLocalizations.of(context)!.joinedPlaylistSnackBar),
         behavior: SnackBarBehavior.floating,
         duration: Duration(milliseconds: 1500),
@@ -202,6 +203,7 @@ class EditPlaylistViewModel extends BaseViewModel {
       this.isChanged = true;
       setPlaylist(playlistUpdate);
       final snackBar = SnackBar(
+        backgroundColor: Theme.of(context).indicatorColor,
         content: Text("Liked Playlist"),
         behavior: SnackBarBehavior.floating,
         duration: Duration(milliseconds: 1500),
@@ -237,6 +239,7 @@ class EditPlaylistViewModel extends BaseViewModel {
         .then((playlistUpdate) {
       setPlaylist(playlistUpdate);
       final snackBar = SnackBar(
+        backgroundColor: Theme.of(context).indicatorColor,
         content: Text(AppLocalizations.of(context)!.playlistUpdateSnackBar),
         behavior: SnackBarBehavior.floating,
         duration: Duration(milliseconds: 1500),
